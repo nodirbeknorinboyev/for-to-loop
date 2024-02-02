14fd50af490b9bd9a7099d6475e0716a7437359f
nodir2208 <norinboyevnortoy544@gmail.com>
2024-02-02 20:06:54 +0500

    fot

---
+++ tk/index.js
@@ -0,0 +1,9 @@
+let a = Number(prompt("Necha Km"));
+
+if(a > 0 && a < 70){
+    console.log("Jarima Miqdori yoq");
+}else if(a > 70 && a < 100){
+    console.log("Extiyot bolimg tezlikni cheklang! ")
+}else if(a > 100){
+    console.log("Sizga Jarima miqdori belgilandi!")
+}
